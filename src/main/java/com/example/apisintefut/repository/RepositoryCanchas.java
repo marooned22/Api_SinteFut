05b1c587d94157b227696ce7de01e62f22cbbb52
AndresSuarez11 <u20221205598@usco.edu.co>
2023-11-20 17:20:53 -0500

    arreglo carpeta

--- src/main/java/com/example/apisintefut/repository/RepositoryCanchas.java
+++
@@ -1,9 +0,0 @@
-package com.example.apisintefut.repository;
-
-import com.example.apisintefut.model.Canchas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryCanchas extends JpaRepository<Canchas, Integer> {
-}
